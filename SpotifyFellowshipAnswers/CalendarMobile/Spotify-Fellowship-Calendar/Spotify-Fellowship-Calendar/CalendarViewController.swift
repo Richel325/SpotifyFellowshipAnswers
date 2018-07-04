d3a551169b20cc1424e43a28b11fd46a442ee887
Richel Cuyler <richelcuyler@icloud.com>
2018-07-04 16:02:47 -0400

    First layouts of calendar UI

---
+++ SpotifyFellowshipAnswers/CalendarMobile/Spotify-Fellowship-Calendar/Spotify-Fellowship-Calendar/CalendarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Spotify-Fellowship-Calendar
+//
+//  Created by Richel Cuyler on 6/28/18.
+//  Copyright © 2018 Richel Cuyler. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
+
---
+++ SpotifyFellowshipAnswers/CalendarMobile/Spotify-Fellowship-Calendar/Spotify-Fellowship-Calendar/MonthSetup.swift
@@ -0,0 +1,9 @@
+//
+//  MonthView.swift
+//  Spotify-Fellowship-Calendar
+//
+//  Created by Richel Cuyler on 7/4/18.
+//  Copyright © 2018 Richel Cuyler. All rights reserved.
+//
+
+import Foundation
